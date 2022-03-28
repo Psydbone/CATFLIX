a5e1d550f508b470dc4597e4f42a5ec7b4332cb1
Psydbone <d.scorcia@studenti.poliba.it>
2022-03-28 17:44:06 +0200

    4 files modified

--- src/page/Home.js
+++ src/page/Home.js
@@ -4,24 +4,13 @@ import '../style.css';
 import TitleHeader from '../components/TitleHeader';
 import VideoList from '../components/VideoList';
 
-export default function Home() {
-  const data = {
-    titleData: {
-      title: "House of Cats",
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      notes: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
-    },
-    lists: ['Continua a guardare','Novità','Commedia'],
-  };
-
-  console.log();
-    data.lists.map((elementocorrente)=> {console.log(elementocorrente)});
- 
-
-  return (
+export default function Home(props) {
+ return (
     <>
-      <TitleHeader titles={data.titleData}/>
-      {data.lists.map((elementocorrente)=> {return <VideoList title={elementocorrente}/>})};
+      <button onClick={() => props.logoutCallback() }>Logout</button> <TitleHeader titles={props.data.titleData} />
+      {props.data.lists.map((elementocorrente) => {
+        return <VideoList title={elementocorrente} />;
+      })}
     </>
   );
 }
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,6 +1,70 @@
 import React from 'react';
 import '../style.css';
 
-export default function Login() {
-  return <h1>Login</h1>;
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  accedi = () => {
+    const emailEsatta = 'pippo@gmail.com';
+    const passwordEsatta = '123';
+    if (
+      this.state.email === emailEsatta &&
+      this.state.password === passwordEsatta
+    ) {
+      console.log('loggato');
+      localStorage.setItem('isLogged',true);
+      this.props.loginCallback();
+      this.setState({ email: '', password: '' });
+    } else {
+      console.log('non loggato');
+    }
+  };
+
+  updateEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  updatePassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="widget">
+          <h1>Accedi</h1>
+          <input
+            type="email"
+            value={this.state.email}
+            placeholder="Email"
+            onChange={this.updateEmail}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            value={this.state.password}
+            placeholder="Password"
+            onChange={this.updatePassword}
+          />
+          <br />
+          <br />
+          <button className="button" onClick={this.accedi}>
+            Accedi
+          </button>
+          <br />
+          <p className="loginNotes">
+            t is a long established fact that a reader will be distracted by the
+            readable content of a page when looking at its layout.
+          </p>
+        </div>
+      </>
+    );
+  }
 }
