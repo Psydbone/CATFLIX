2897f1b40f9933b5bbdc05ce9f13d62a7e4c582d
Psydbone <d.scorcia@studenti.poliba.it>
2022-03-28 11:48:47 +0200

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Home from './page/Home';
+//import Login from './page/Login';
+//import Player from './page/Player'
 import './style.css';
 
 export default function App() {
